9f0da8e6472b37940829cac23dd8fbacef6ae199
MonisBana <monisbana97@gmail.com>
2017-08-28 02:08:59 +0530

    Monis
    Sign in+SignUp+DataEntry Page Added

---
+++ app/src/main/java/com/csi/csi_organiser/Model2.java
@@ -0,0 +1,39 @@
+package com.csi.csi_organiser;
+
+/**
+ * Created by montur on 8/27/2017.
+ */
+public class Model2 {
+    public  String rollno;
+    public String number;
+    public String priority="1";
+
+
+
+
+    public void setValues(String rollno, String number)
+    {
+
+        this.rollno=rollno;
+        this.number=number;
+    }
+
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public String getRollno() {return rollno;}
+
+    public void setRollno(String rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
